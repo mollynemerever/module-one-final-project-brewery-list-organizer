2ef93f353c46b1e541e5888fe35bee65fda06301
github username <github email address>
2019-03-05 15:48:09 -0800

    Added to seeds.rb file-- iterating through the pagated API in order to populate our seed file with all of the available breweries.

--- db/seeds.rb
+++ db/seeds.rb
@@ -4,11 +4,14 @@ require 'rest-client'
 require 'json'
 require 'pry'
 require_relative "../config/environment.rb"
-
-response_string = RestClient.get("https://api.openbrewerydb.org/breweries"
-)
-json = JSON.parse(response_string)
-json.each do |brewery|
+url = "https://api.openbrewerydb.org/breweries?page="
+counter = 1
+until counter == 175 do
+  response_string = RestClient.get("#{url}#{counter}&per_page=50")
+  json = JSON.parse(response_string)
+  json.each do |brewery|
+  end
+  counter += 1
   binding.pry
 end
 
