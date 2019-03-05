b4cd1a1eca7e86a391cd705bfab9b6bc0fecb4de
github username <github email address>
2019-03-05 12:56:39 -0800

    added seeds.rb file

--- app/models/brewery_wishlist.rb
+++ app/models/brewery_wishlist.rb
@@ -1,1 +1,7 @@
-#this file holds the class for brewery wishlist 
+class Brewery_Wishlist < ActiveRecord::Base
+
+#method to display brewery wishlist 
+
+end
+
+#this file holds the class for brewery wishlist
--- db/migrate/001_create_brewerys_table.rb
+++ db/migrate/001_create_brewerys_table.rb
@@ -1,3 +1,16 @@
 class CreateBrewerysTable < ActiveRecord::Migration[4.2]
 
+  def change
+    create_table :brewery do |t|
+      t.string :name
+      t.string :brewery_type
+      t.string :street
+      t.string :city
+      t.string :state
+      t.string :postal_code
+      t.string :phone
+      t.string :website_url
+    end
+  end
+
 end
