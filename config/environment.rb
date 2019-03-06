7878bf58277c977e9a1568879596008e17be48bf
github username <github email address>
2019-03-05 17:44:54 -0800

    create new user complete

--- config/environment.rb
+++ config/environment.rb
@@ -10,5 +10,5 @@ require 'json'
 Bundler.require
 
 ActiveRecord::Base.establish_connection(adapter: 'sqlite3', database: 'db/development.db')
-require_all 'lib'
 require_all 'app'
+require_all 'lib'
