5916b64ab91f623f05e149e242f57f5fc3b24dcf
github username <github email address>
2019-03-06 10:30:46 -0800

    search by city state name methods

--- lib/command_line_interface.rb
+++ lib/command_line_interface.rb
@@ -11,6 +11,29 @@ require_relative "../config/environment.rb"
 # t.string :favorite_beer
 puts "test"
 
+
+def search_breweries_by_city
+  puts "What city do you want to search by? (capitalize first letter)"
+  city_input = gets.chomp #go back and revise to acoomodate up/downcase
+  breweries_by_city = Brewery.where(city: city_input)
+  breweries_by_city.type 
+end
+
+
+def search_breweries_by_state
+  puts "What state do you want to search by? (full name & capitalization)"
+  state_input = gets.chomp #go back and revise to accomodate abbreviations
+  breweries_by_state = Brewery.where(state: state_input)
+  breweries_by_state
+end
+
+def search_breweries_by_name
+  puts "What's the brewery name you want to search for?"
+  name_input = gets.chomp #go back and revise to acoomodate abbreviations
+  breweries_by_name = Brewery.where(name: name_input)
+  breweries_by_name
+end
+
 def create_new_user
   puts "Please fill out the following account info:"
   puts "Username?"
@@ -39,4 +62,3 @@ def create_new_user
 
 end
 Pry.start
-
