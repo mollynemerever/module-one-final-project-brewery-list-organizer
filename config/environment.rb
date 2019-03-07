c88f221c881a4c839bcbcd7c1e38e2b3465d444d
github username <github email address>
2019-03-07 13:15:30 -0800

    Ironing out all the kinks

--- config/environment.rb
+++ config/environment.rb
@@ -2,6 +2,7 @@ require 'bundler'
 Bundler.require
 
 ActiveRecord::Base.establish_connection(adapter: 'sqlite3', database: 'db/development.db')
-ActiveRecord::Base.logger = nil
+ActiveRecord::Base.logger = nil #removes extrenuous return values when running program.
+
 require_all 'app'
 require_all 'lib'
